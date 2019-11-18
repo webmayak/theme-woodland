e7b8d3d02d50000dc0c7d5b4c7d996282ee49f66
aileen <aileen.kingsler@mail.ru>
2019-11-18 13:33:15 +1000

    Адаптив блока Наши преимущества

--- views/content/view/index--front.php
+++ views/content/view/index--front.php
@@ -480,52 +480,66 @@ $this->context->layout = '//front';
 <div class="advantages">
     <div class="container">
         <h2 class="advantages__title section-title">Наши преимущества</h2>
-        <ul class="advantages-list ul-reset">
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="29" fill="currentColor">
-                        <use xlink:href="/images/sprite.svg#icon-address">
-                    </svg>
+        <div class="row justify-content-center no-gutters">
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="29" fill="currentColor">
+                            <use xlink:href="/images/sprite.svg#icon-address">
+                        </svg>
+                    </div>
+                    <div class="advantage__descr">Офисы в Москве и&nbsp;Волгограде</div>
                 </div>
-                <div class="advantage__descr">Офисы в Москве и&nbsp;Волгограде</div>
-            </li>
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <img src="/images/advantage-2.png" alt="">
+            </div>
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <img src="/images/advantage-2.png" alt="">
+                    </div>
+                    <div class="advantage__descr">Собственный склад круглого леса</div>
                 </div>
-                <div class="advantage__descr">Собственный склад круглого леса</div>
-            </li>
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <img src="/images/advantage-3.png" alt="">
+            </div>
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <img src="/images/advantage-3.png" alt="">
+                    </div>
+                    <div class="advantage__descr">Современная пилорама</div>
                 </div>
-                <div class="advantage__descr">Современная пилорама</div>
-            </li>
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <img src="/images/advantage-4.png" alt="">
+            </div>
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <img src="/images/advantage-4.png" alt="">
+                    </div>
+                    <div class="advantage__descr">Цеха по обработке дерева</div>
                 </div>
-                <div class="advantage__descr">Цеха по обработке дерева</div>
-            </li>
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <img src="/images/advantage-5.png" alt="">
+            </div>
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <img src="/images/advantage-5.png" alt="">
+                    </div>
+                    <div class="advantage__descr">Склад готовой продукции</div>
                 </div>
-                <div class="advantage__descr">Склад готовой продукции</div>
-            </li>
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <img src="/images/advantage-6.png" alt="">
+            </div>
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <img src="/images/advantage-6.png" alt="">
+                    </div>
+                    <div class="advantage__descr">Собственный парк автотранспорта</div>
                 </div>
-                <div class="advantage__descr">Собственный парк автотранспорта</div>
-            </li>
-            <li class="advantages-list__item advantage">
-                <div class="advantage__circle">
-                    <img src="/images/advantage-7.png" alt="">
+            </div>
+            <div class="col-xl col-md-3 col-sm-4 col-6">
+                <div class="advantages__item advantage">
+                    <div class="advantage__circle">
+                        <img src="/images/advantage-7.png" alt="">
+                    </div>
+                    <div class="advantage__descr">Высокое качество продукции</div>
                 </div>
-                <div class="advantage__descr">Высокое качество продукции</div>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </div>
 
