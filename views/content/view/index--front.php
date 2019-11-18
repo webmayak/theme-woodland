7288af42721b03e884db81b900f4b0b205a66f4d
aileen <aileen.kingsler@mail.ru>
2019-11-18 14:00:15 +1000

    Адаптив блока Как мы работаем

--- views/content/view/index--front.php
+++ views/content/view/index--front.php
@@ -442,38 +442,50 @@ $this->context->layout = '//front';
 <div class="steps">
     <div class="container">
         <h2 class="steps__title section-title">Как мы работаем</h2>
-        <ul class="steps-list ul-reset">
-            <li class="steps-list__item step">
-                <div class="step__number">01</div>
-                <div class="step__title">Общение</div>
-                <div class="step__descr">Общение с заказчиком, получение информации для отрисовки и просчёта проекта</div>
-            </li>
-            <li class="steps-list__item step">
-                <div class="step__number">02</div>
-                <div class="step__title">Создание</div>
-                <div class="step__descr">Создание<br> проекта и технической документации</div>
-            </li>
-            <li class="steps-list__item step">
-                <div class="step__number">03</div>
-                <div class="step__title">Согласовываем</div>
-                <div class="step__descr">Согласовываем<br> и подписываем документы +&nbsp;предоплата 35%</div>
-            </li>
-            <li class="steps-list__item step">
-                <div class="step__number">04</div>
-                <div class="step__title">Встреча</div>
-                <div class="step__descr">Встреча заказчика<br> с прорабом на участке и обсуждение вопросов</div>
-            </li>
-            <li class="steps-list__item step">
-                <div class="step__number">05</div>
-                <div class="step__title">Вывоз</div>
-                <div class="step__descr">Вывоз домокомплекта,<br> начало работ бригады +&nbsp;вторая оплата 35%</div>
-            </li>
-            <li class="steps-list__item step">
-                <div class="step__number">06</div>
-                <div class="step__title">Приемка</div>
-                <div class="step__descr">Приемка проекта согласно документации, заказчиком и  прорабом +&nbsp;остаток оплаты 30%</div>
-            </li>
-        </ul>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 col-sm-6">
+                <div class="steps__item step">
+                    <div class="step__number">01</div>
+                    <div class="step__title" style="background-image: url('/images/1.svg')">Общение</div>
+                    <div class="step__descr">Общение с заказчиком, получение информации для отрисовки и просчёта проекта</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 col-sm-6">
+                <div class="steps__item step">
+                    <div class="step__number">02</div>
+                    <div class="step__title" style="background-image: url('/images/2.svg')">Создание</div>
+                    <div class="step__descr">Создание<br> проекта и технической документации</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="steps__item step">
+                    <div class="step__number">03</div>
+                    <div class="step__title" style="background-image: url('/images/3.svg')">Согласовываем</div>
+                    <div class="step__descr">Согласовываем<br> и подписываем документы +&nbsp;предоплата 35%</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="steps__item step">
+                    <div class="step__number">04</div>
+                    <div class="step__title" style="background-image: url('/images/4.svg')">Встреча</div>
+                    <div class="step__descr">Встреча заказчика<br> с прорабом на участке и обсуждение вопросов</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="steps__item step">
+                    <div class="step__number">05</div>
+                    <div class="step__title" style="background-image: url('/images/5.svg')">Вывоз</div>
+                    <div class="step__descr">Вывоз домокомплекта,<br> начало работ бригады +&nbsp;вторая оплата 35%</div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="steps__item step">
+                    <div class="step__number">06</div>
+                    <div class="step__title" style="background-image: url('/images/6.svg')">Приемка</div>
+                    <div class="step__descr">Приемка проекта согласно документации, заказчиком и  прорабом +&nbsp;остаток оплаты 30%</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
