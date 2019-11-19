cf96f85df4d32d733c714d9b85665ba6ff32bc73
aileen <aileen.kingsler@mail.ru>
2019-11-19 13:40:53 +1000

    Добавила fancybox для фото работ

--- views/content/view/index--front.php
+++ views/content/view/index--front.php
@@ -867,24 +867,24 @@ $this->context->layout = '//front';
         <h2 class="works__title section-title">Наши выполненные работы</h2>
         <div class="row">
             <div class="col-md-3 col-sm-6">
-                <div class="works__item">
+                <a class="works__item" href="/images/work-1.jpg" data-fancybox="works">
                     <img src="/images/work-1.jpg" alt="">
-                </div>
+                </a>
             </div>
             <div class="col-md-3 col-sm-6">
-                <div class="works__item">
+                <a class="works__item" href="/images/work-2.jpg" data-fancybox="works">
                     <img src="/images/work-2.jpg" alt="">
-                </div>
+                </a>
             </div>
             <div class="col-md-3 col-sm-6">
-                <div class="works__item">
+                <a class="works__item" href="/images/work-3.jpg" data-fancybox="works">
                     <img src="/images/work-3.jpg" alt="">
-                </div>
+                </a>
             </div>
             <div class="col-md-3 col-sm-6">
-                <div class="works__item">
+                <a class="works__item" href="/images/work-4.jpg" data-fancybox="works">
                     <img src="/images/work-4.jpg" alt="">
-                </div>
+                </a>
             </div>
         </div>
     </div>
