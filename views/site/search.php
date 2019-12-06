c2cb88bb6ca749d00efaadb5b41a22321da327bc
Alex Kostin <web-mayak@mail.ru>
2019-12-06 17:03:46 +1000

    базово верстка страницы "Поиск по сайту"

--- views/site/search.php
+++ views/site/search.php
@@ -11,9 +11,32 @@ use yii\web\View;
 
 /* @var $this View */
 /* @var $links array */
-$this->title = 'Поиск по сайту';
+$this->title = '"Дом" - результаты поиска';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h1 class="title-home">
     <?= $this->title ?>
 </h1>
+
+<div style="margin: 30px 0;">
+	<div class="input-group">
+		<input class="form-control form-control-lg" type="text" placeholder="Поиск по сайту" value="Дом">
+		<div class="input-group-append">
+          <button type="submit" class="btn btn-primary" style="width: 160px;">Поиск</button>
+        </div>
+	</div>
+</div>
+
+<div style="margin: 30px 0 20px;">Если результаты вас не удовлетворяют, пожалуйста, попробуйте еще раз</div>
+
+<br/>
+
+<?= $this->render('@theme/views/_catalog_grid') ?>
+
+<br/>
+
+<div class="text-center">
+    <button class="btn btn-lg btn-primary">Показать еще</button>
+</div>
+
+<br/><br/>
