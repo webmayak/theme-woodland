5d52587c67603e1ae6661d1b5eb9ae64736890ae
aileen <aileen.kingsler@mail.ru>
2019-12-06 16:01:43 +1000

    Сверстала карту сайта

--- views/site/sitemap.php
+++ views/site/sitemap.php
@@ -14,46 +14,40 @@ use yii\web\View;
 $this->title = 'Карта сайта';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<h1 class="title-home">
-    <?= $this->title ?>
-</h1>
-<div class="row">
-	<div class="col-md-4">
-		<div class="h2">Разделы</div>
-		<ul>
-		    <?php foreach ($links1 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-		<div class="h2">Статьи</div>
-		<ul>
-		    <?php foreach ($links2 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-	</div>
-	<div class="col-md-4">
-		<div class="h2">Каталог</div>
-		<ul>
-		    <?php foreach ($links3 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-	</div>
-	<div class="col-md-4">
-		<div class="h2">Акции и скидки</div>
-		<ul>
-		    <?php foreach ($links4 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-	</div>
-</div>
+<main class="page-sitemap__content">
+    <h1 class="title-home">
+        <?= $this->title ?>
+    </h1>
+    <div class="page-sitemap__title">Разделы</div>
+    <ul>
+        <?php foreach ($links1 as $link) : ?>
+            <li>
+                <?= Html::a($link['anchor'], $link['url']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <div class="page-sitemap__title">Статьи</div>
+    <ul>
+        <?php foreach ($links2 as $link) : ?>
+            <li>
+                <?= Html::a($link['anchor'], $link['url']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <div class="page-sitemap__title">Каталог</div>
+    <ul>
+        <?php foreach ($links3 as $link) : ?>
+            <li>
+                <?= Html::a($link['anchor'], $link['url']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <div class="page-sitemap__title">Акции и скидки</div>
+    <ul>
+        <?php foreach ($links4 as $link) : ?>
+            <li>
+                <?= Html::a($link['anchor'], $link['url']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</main>
