ea28dd738bf96bb25abd841df0ebf025273ffde4
Alex Kostin <web-mayak@mail.ru>
2019-12-06 16:38:04 +1000

    базово верстка страницы 404

--- views/site/error.php
+++ views/site/error.php
@@ -7,26 +7,26 @@
 
 use yii\helpers\Html;
 
-$this->title = $name;
+//$this->title = $name;
+$this->title = 'Страница не найдена';
+$this->params['breadcrumbs'][] = 'Страница 404';
+
 ?>
 
-<h1>Упс! <?= $exception->statusCode . ' ' . Html::encode($message) ?>..</h1>
+<?php if (0): ?><h1>Упс! <?= $exception->statusCode . ' ' . Html::encode($message) ?>..</h1><?php endif; ?>
+<h1><?= Html::encode($this->title) ?></h1>
 
-<div class="error">
+<div class="text-center">
 
-    <div class="h3">Без паники!</div>
-    
-    <p>Возможно запрашиваемая страница была перемещена по новому адресу в связи с обновлением сайта.</p>
-
-    <p>В любом случае наши специалисты уже знают об этом и занимаются решением проблемы!</p>
-
-    <br/>
-
-    <a class="btn btn-lg btn-default" href="/catalog">Перейти в каталог</a>
-
-    &nbsp;
-
-    <a class="btn btn-lg btn-primary" href="/">На главную страницу »</a>
+    <p>
+    	<img style="max-width: 100%;" src="/images/404.png" alt="Страница не найдена" />
+    </p>
+    <p style="font-size: 16px; margin: 60px 0 30px;">
+		Приносим извинения, но такой страницы не существует
+	</p>
+	<p>
+	    <a class="btn btn-lg btn-success" href="/">Вернуться на главную</a>
+	</p>
 </div>
 
 <br/><br/>
