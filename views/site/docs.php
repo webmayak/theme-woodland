784a2fc14e34117361be1bf5d742baaa19962906
aileen <aileen.kingsler@mail.ru>
2019-12-09 15:36:10 +1000

    Добавлены вьюшки для Галерея, Документы

---
+++ views/site/docs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: singletonn
+ * Date: 10/31/18
+ * Time: 4:40 PM
+ */
+
+use yii\helpers\Html;
+use yii\web\View;
+
+/* @var $this View */
+/* @var $links array */
+$this->title = 'Документы';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<h1 class="title-home">
+    <?= $this->title ?>
+</h1>
