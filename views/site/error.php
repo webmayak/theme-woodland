13fedb5cfa79e455bf1e0204d0d8857086350581
aileen <aileen.kingsler@mail.ru>
2019-12-09 15:15:31 +1000

    Доработала страницу поиска и 404 ошибки

--- views/site/error.php
+++ views/site/error.php
@@ -16,17 +16,8 @@ $this->params['breadcrumbs'][] = 'Страница 404';
 <?php if (0): ?><h1>Упс! <?= $exception->statusCode . ' ' . Html::encode($message) ?>..</h1><?php endif; ?>
 <h1><?= Html::encode($this->title) ?></h1>
 
-<div class="text-center">
-
-    <p>
-    	<img style="max-width: 100%;" src="/images/404.png" alt="Страница не найдена" />
-    </p>
-    <p style="font-size: 16px; margin: 60px 0 30px;">
-		Приносим извинения, но такой страницы не существует
-	</p>
-	<p>
-	    <a class="btn btn-lg btn-success" href="/">Вернуться на главную</a>
-	</p>
-</div>
-
-<br/><br/>
+<main class="page-404__content text-center">
+    <img class="page-404__image" src="/images/404.png" alt="Страница не найдена">
+    <div class="page-404__text">Приносим извинения, но такой страницы не существует</div>
+	<a class="btn btn-lg btn-success" href="/">Вернуться на главную</a>
+</main>
--- views/site/search.php
+++ views/site/search.php
@@ -18,25 +18,42 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $this->title ?>
 </h1>
 
-<div style="margin: 30px 0;">
+<main class="page-search__content">
 	<div class="input-group">
-		<input class="form-control form-control-lg" type="text" placeholder="Поиск по сайту" value="Дом">
+		<input class="page-search__search-field form-control form-control-lg" type="text" placeholder="Поиск по сайту" value="Дом">
 		<div class="input-group-append">
-          <button type="submit" class="btn btn-primary" style="width: 160px;">Поиск</button>
+          <button type="submit" class="page-search__search-btn btn btn-primary">Поиск</button>
         </div>
 	</div>
-</div>
-
-<div style="margin: 30px 0 20px;">Если результаты вас не удовлетворяют, пожалуйста, попробуйте еще раз</div>
-
-<br/>
-
-<?= $this->render('@theme/views/_catalog_grid') ?>
-
-<br/>
-
-<div class="text-center">
-    <button class="btn btn-lg btn-primary">Показать еще</button>
-</div>
-
-<br/><br/>
+    <div class="page-search__text">Если результаты вас не удовлетворяют, пожалуйста, попробуйте еще раз</div>
+    <?= $this->render('@theme/views/_catalog_grid') ?>
+    <div class="page-search__btn-wrap text-center">
+        <button class="btn btn-lg btn-primary">Показать еще</button>
+    </div>
+    <ul class="page-search__pagination pagination justify-content-center align-items-center">
+        <li class="page-item">
+            <a class="page-link" href="#"><i class="fa fa-angle-left"></i> Назад</a>
+        </li>
+        <li class="page-item active">
+            <a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="page-item">
+            <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+            <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+            <a class="page-link" href="#">4</a>
+        </li>
+        <li class="page-item">
+            <a class="page-link">...</a>
+        </li>
+        <li class="page-item">
+            <a class="page-link" href="#">7</a>
+        </li>
+        <li class="page-item">
+            <a class="page-link" href="#">Вперед <i class="fa fa-angle-right"></i></a>
+        </li>
+    </ul>
+</main>
