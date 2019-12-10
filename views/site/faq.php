8cb623358dc9d0be9186a73b1296490da3406f88
aileen <aileen.kingsler@mail.ru>
2019-12-10 12:03:29 +1000

    Поправила разметку на странице FAQ во втором блоке

--- views/site/faq.php
+++ views/site/faq.php
@@ -95,8 +95,10 @@ $this->params['breadcrumbs'][] = $this->title;
                                 Как будет происходить работа по моему заказу?
                             </button>
                         </div>
-                        <div id="accordion-2-1" class="faq-item__answer collapse" data-parent="#accordion-2">
-                            <p>Наша компания основана в Росии. У нас есть офисы, расположенные в Москве, Новосибирске и Омске, чтобы обеспечить максимальный комформ для Вас, мы приглашает в наш офис, где наши менеджерыпомогут Вам выбрать проект дома. Оформить заявку можно так же по ссылке http://fortuna-les.ru/</p>
+                        <div id="accordion-2-1" class="collapse" data-parent="#accordion-2">
+                            <div class="faq-item__answer">
+                                <p>Наша компания основана в Росии. У нас есть офисы, расположенные в Москве, Новосибирске и Омске, чтобы обеспечить максимальный комформ для Вас, мы приглашает в наш офис, где наши менеджерыпомогут Вам выбрать проект дома. Оформить заявку можно так же по ссылке http://fortuna-les.ru/</p>
+                            </div>
                         </div>
                     </div>
                     <div class="faq-item">
@@ -105,10 +107,10 @@ $this->params['breadcrumbs'][] = $this->title;
                                 Каким образом будет производиться оплата?
                             </button>
                         </div>
-                        <div id="accordion-2-2" class="faq-item__answer collapse" data-parent="#accordion-2">
-                            <p>
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </p>
+                        <div id="accordion-2-2" class="collapse" data-parent="#accordion-2">
+                            <div class="faq-item__answer">
+                                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+                            </div>
                         </div>
                     </div>
                     <div class="faq-item">
@@ -117,10 +119,10 @@ $this->params['breadcrumbs'][] = $this->title;
                                 Требуется ли предоплата?
                             </button>
                         </div>
-                        <div id="accordion-2-3" class="faq-item__answer collapse" data-parent="#accordion-2">
-                            <p>
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </p>
+                        <div id="accordion-2-3" class="collapse" data-parent="#accordion-2">
+                            <div class="faq-item__answer">
+                                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
