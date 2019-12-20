da9d14f2a1280d080556675bf6f1397dc2b986c6
Alex Kostin <web-mayak@mail.ru>
2019-12-20 19:04:02 +1000

    подключена верстка: каталог, категория, страница товара

---
+++ views/shop/catalog/category.php
@@ -0,0 +1,40 @@
+<?php
+
+use common\modules\shop\widgets\productFilter\ProductFilter;
+use common\modules\shop\widgets\productFilter\ProductFilterWidgetConfiguration;
+use common\modules\shop\widgets\productsListWidget\ProductsListWidget;
+use common\modules\shop\widgets\productsListWidget\ProductsListWidgetConfiguration;
+
+$this->title = $model->name;
+
+$this->params['breadcrumbs'][] = ['label' => 'Каталог', 'url' => '/shop/catalog'];
+foreach ($model->parents as $key => $parent) {
+    if ($key == 0) continue;
+    $this->params['breadcrumbs'][] = ['label' => $parent->name, 'url' => $parent->present()->getUrl()];
+}
+$this->params['breadcrumbs'][] = $this->title;
+
+$dataProvider->pagination = ['defaultPageSize' => 12];
+
+/**
+ * @var $dataProvider \yii\data\ActiveDataProvider
+ */
+?><h1><?=$this->title?></h1>
+
+<?= \yii\widgets\ListView::widget([
+    'dataProvider' => $dataProvider,
+    'options' => [
+        'class' => 'products-list',
+    ],
+    'itemView' => '@theme/views/_product-card',
+    'itemOptions' => [
+        'class' => 'col-xl-3 col-lg-4 col-sm-6',
+    ],
+    'layout' => '<div class="row">{items}</div>{pager}',
+    'pager' => [
+        'class' => 'yii\bootstrap4\LinkPager',
+        'options' => [
+            'class' => 'text-center',
+        ],
+    ],
+]) ?>
---
+++ views/shop/catalog/index.php
@@ -0,0 +1,95 @@
+<?php
+
+use pantera\leads\widgets\form\LeadForm;
+use yii\web\View;
+
+/* @var $this View */
+/* @var $categories array */
+
+$this->title = "Каталог";
+$this->params['breadcrumbs'][] = $this->title;
+
+?><h1><?=$this->title?></h1>
+
+<?php if ($categories) : ?>
+    <ul class="catalog-page__nav-tabs nav nav-tabs" role="tablist">
+    <?php foreach ($categories as $key => $category): ?>
+        <li class="nav-item">
+            <a class="nav-link<?= $key == 0 ? ' active' : ''?>" data-toggle="tab" href="#catalog-tab-<?= $key ?>" style="white-space: nowrap;"><?= $category->name ?></a>
+            <?php if (0): ?>
+                <?php if ($lvl2cats = $category->getChildren()->andWhere(['status' => 1])->all()): ?>
+                    <ul>
+                    <?php foreach ($lvl2cats as $lvl2cat): ?>
+                        <li>
+                            <a href="<?= $lvl2cat->present()->getUrl() ?>"><?= $lvl2cat->name ?></a>
+                            <?php if ($lv3categories = $lvl2cat->getChildren()->andWhere(['status' => 1])->all()): ?>
+                            <ul>
+                                <?php foreach ($lv3categories as $lvl3cat): ?>
+                                    <li><a href="<?= $lvl3cat->present()->getUrl() ?>"><?= $lvl3cat->name ?></a></li>
+                                <?php endforeach; ?>
+                            </ul>
+                            <?php endif; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php endif; ?>
+            <?php endif; ?>
+        </li>
+    <?php endforeach;?>
+    </ul>
+<?php endif; ?>
+
+<?php if ($categories) : ?>
+    <div class="catalog-page__tab-content tab-content">
+        <?php foreach ($categories as $key => $category) : ?>
+            <div class="tab-pane fade show<?= $key == 0 ? ' active' : ''?>" id="catalog-tab-<?= $key ?>">
+                <?= \yii\widgets\ListView::widget([
+                    'dataProvider' => new \yii\data\ActiveDataProvider([
+                        'query' => \common\modules\shop\models\ShopProduct::find()->andWhere(['category_id' => $category->id])
+                    ]),
+                    'options' => [
+                        'class' => 'products-list',
+                    ],
+                    'itemView' => '@theme/views/_product-card',
+                    'itemOptions' => [
+                        'class' => 'col-xl-3 col-lg-4 col-sm-6',
+                    ],
+                    'layout' => '<div class="row">{items}</div>{pager}',
+                    'pager' => [
+                        'class' => 'yii\bootstrap4\LinkPager',
+                        'options' => [
+                            'class' => 'text-center',
+                        ],
+                    ],
+                ]) ?>
+                <div class="catalog-page__btn-wrap">
+                    <?php if (0) : ?>
+                        <button class="catalog-page__btn btn btn-lg btn-primary">Показать еще</button>
+                    <?php endif; ?>
+                    <a class="catalog-page__btn btn btn-lg btn-success" href="<?= $category->present()->getUrl() ?>">Перейти в категорию</a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+
+</div><!-- закрываем .container -->
+
+<?= $this->render('@theme/views/_advantages') ?>
+
+<?= $this->render('@theme/views/_steps') ?>
+
+<div class="contact-form-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-7 col-lg-8">
+                <?= LeadForm::widget([
+                    'key' => 'request',
+                    'mode' => LeadForm::MODE_INLINE,
+                ]) ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container"><!-- открываем снова .container -->
