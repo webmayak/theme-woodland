023320f9bd428e035abd2bc148a2e8bc17aa3b20
aileen <aileen.kingsler@mail.ru>
2019-12-25 12:50:08 +1000

    Вывела ссылки из админки в главное меню

--- widgets/megaMenu/views/index.php
+++ widgets/megaMenu/views/index.php
@@ -23,10 +23,11 @@ use yii\web\View;
         <nav class="wsmenu">
             <ul class="wsmenu-list">
                 <?php
-                $catalogIsActive = preg_match('/^catalog/', Yii::$app->request->pathInfo);
+                $catalogIsActive = preg_match('/catalog/', Yii::$app->request->pathInfo);
                 $brandsIsActive = preg_match('/^brands/', Yii::$app->request->pathInfo);
                 ?>
-                <li class="<?= $catalogIsActive ? 'active' : '' ?>active" id="main-menu-catalog">
+
+                <li class="<?= $catalogIsActive ? 'active' : '' ?>" id="main-menu-catalog">
                     <a href="<?= Url::to(['/shop/catalog']) ?>" class="navtext vsmenu-cat-toggle">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-address"/>
@@ -58,14 +59,17 @@ use yii\web\View;
                     ]) ?>
                     <?php $this->endCache(); endif; ?>
                 </li>
+
                 <?php if ($parent = \common\modules\catalog\models\CatalogCategory::findOne(241)) : ?>
-                <li class="<?= false ? 'active' : '' ?>">
+                <li class="<?= Yii::$app->request->pathInfo === $parent->slug ? 'active' : '' ?>">
                     <a class="navtext" href="<?= $parent->present()->getUrl() ?>">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-wall"/>
                         </svg>
                         <?= Html::encode($parent->name) ?>
-                        <i class="fa fa-chevron-down"></i>
+                        <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                            <i class="fa fa-chevron-down"></i>
+                        <?php endif; ?>
                     </a>
                     <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
                         <ul class="sub-menu">
@@ -78,87 +82,122 @@ use yii\web\View;
                     <?php endif; ?>
                 </li>
                 <?php endif; ?>
-                <li class="<?= Yii::$app->request->pathInfo === 'gallery' ? 'active' : '' ?>">
-                    <a class="navtext" href="<?= Url::to(['/site/gallery']) ?>">
+
+                <?php if ($parent = \common\modules\catalog\models\CatalogCategory::findOne(235)) : ?>
+                <li class="<?= Yii::$app->request->pathInfo === $parent->slug ? 'active' : '' ?>">
+                    <a class="navtext" href="<?= $parent->present()->getUrl() ?>">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-gallery"/>
                         </svg>
-                        Галерея
+                        <?= Html::encode($parent->name) ?>
+                        <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                            <i class="fa fa-chevron-down"></i>
+                        <?php endif; ?>
                     </a>
+                    <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                        <ul class="sub-menu">
+                            <?php foreach ($childs as $child): ?>
+                                <li>
+                                    <?= Html::a($child->name, $child->present()->getUrl()) ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
                 </li>
-                <li class="<?= false ? 'active' : '' ?>">
-                    <a class="navtext" href="<?= Url::to(['#']) ?>">
+                <?php endif; ?>
+
+                <?php if ($parent = \common\modules\catalog\models\CatalogCategory::findOne(237)) : ?>
+                <li class="<?= Yii::$app->request->pathInfo === $parent->slug ? 'active' : '' ?>">
+                    <a class="navtext" href="<?= $parent->present()->getUrl() ?>">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-company"/>
                         </svg>
-                        О компании
-                        <i class="fa fa-chevron-down"></i>
+                        <?= Html::encode($parent->name) ?>
+                        <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                            <i class="fa fa-chevron-down"></i>
+                        <?php endif; ?>
                     </a>
-                    <ul class="sub-menu">
-                        <li><a href="<?= Url::to(['/news']) ?>">Новости</a></li>
-                        <li><a href="<?= Url::to(['/articles']) ?>">Статьи</a></li>
-                        <li><a href="<?= Url::to(['/specials']) ?>">Акции</a></li>
-                        <li><a href="<?= Url::to(['/site/reviews']) ?>">Отзывы</a></li>
-                        <li><a href="<?= Url::to(['/site/faq']) ?>">FAQ</a></li>
-                        <li><a href="<?= Url::to(['/site/docs']) ?>">Документы</a></li>
-                    </ul>
+                    <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                        <ul class="sub-menu">
+                            <?php foreach ($childs as $child): ?>
+                                <li>
+                                    <?= Html::a($child->name, $child->present()->getUrl()) ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
                 </li>
-                <li class="<?= false ? 'active' : '' ?>">
-                    <a class="navtext" href="<?= Url::to(['#']) ?>">
+                <?php endif; ?>
+
+                <?php if ($parent = \common\modules\catalog\models\CatalogCategory::findOne(238)) : ?>
+                <li class="<?= Yii::$app->request->pathInfo === $parent->slug ? 'active' : '' ?>">
+                    <a class="navtext" href="<?= $parent->present()->getUrl() ?>">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-percentage"/>
                         </svg>
-                        Покупателю
-                        <i class="fa fa-chevron-down"></i>
+                        <?= Html::encode($parent->name) ?>
+                        <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                            <i class="fa fa-chevron-down"></i>
+                        <?php endif; ?>
                     </a>
-                    <ul class="sub-menu">
-                        <li>
-                            <a href="<?= Url::to(['#']) ?>">Пункт 1</a>
-                        </li>
-                        <li>
-                            <a href="<?= Url::to(['#']) ?>">Пункт 2</a>
-                        </li>
-                    </ul>
+                    <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                        <ul class="sub-menu">
+                            <?php foreach ($childs as $child): ?>
+                                <li>
+                                    <?= Html::a($child->name, $child->present()->getUrl()) ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
                 </li>
-                <li class="<?= false ? 'active' : '' ?>">
-                    <a class="navtext" href="<?= Url::to(['#']) ?>">
+                <?php endif; ?>
+
+                <?php if ($parent = \common\modules\catalog\models\CatalogCategory::findOne(234)) : ?>
+                <li class="<?= Yii::$app->request->pathInfo === $parent->slug ? 'active' : '' ?>">
+                    <a class="navtext" href="<?= $parent->present()->getUrl() ?>">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-fence"/>
                         </svg>
-                        Услуги
-                        <i class="fa fa-chevron-down"></i>
+                        <?= Html::encode($parent->name) ?>
+                        <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                            <i class="fa fa-chevron-down"></i>
+                        <?php endif; ?>
                     </a>
-                    <ul class="sub-menu">
-                        <li>
-                            <a href="<?= Url::to(['#']) ?>">Благоустройства</a>
-                            <ul class="sub-menu">
+                    <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                        <ul class="sub-menu">
+                            <?php foreach ($childs as $child): ?>
                                 <li>
-                                    <a href="<?= Url::to(['#']) ?>">Газоны</a>
+                                    <?= Html::a($child->name, $child->present()->getUrl()) ?>
                                 </li>
-                                <li>
-                                    <a href="<?= Url::to(['#']) ?>">Бассейны</a>
-                                </li>
-                                <li>
-                                    <a href="<?= Url::to(['#']) ?>">Газоны</a>
-                                </li>
-                                <li>
-                                    <a href="<?= Url::to(['#']) ?>">Бассейны</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="<?= Url::to(['#']) ?>">Ворота</a>
-                        </li>
-                    </ul>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
                 </li>
-                <li class="<?= Yii::$app->request->pathInfo === 'kontakty' ? 'active' : '' ?>">
-                    <a class="navtext" href="<?= Url::to(['/kontakty']) ?>">
+                <?php endif; ?>
+
+                <?php if ($parent = \common\modules\catalog\models\CatalogCategory::findOne(243)) : ?>
+                <li class="<?= Yii::$app->request->pathInfo === $parent->slug ? 'active' : '' ?>">
+                    <a class="navtext" href="<?= $parent->present()->getUrl() ?>">
                         <svg class="megamenu__icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
                             <use xlink:href="/images/sprite.svg#icon-city"/>
                         </svg>
-                        Контакты
+                        <?= Html::encode($parent->name) ?>
+                        <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                            <i class="fa fa-chevron-down"></i>
+                        <?php endif; ?>
                     </a>
+                    <?php if ($childs = $parent->getChildren()->isInMenu()->isActive()->all()) : ?>
+                        <ul class="sub-menu">
+                            <?php foreach ($childs as $child): ?>
+                                <li>
+                                    <?= Html::a($child->name, $child->present()->getUrl()) ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
                 </li>
+                <?php endif; ?>
+
                 <li>
                     <a class="navicon" href="<?= Url::to(['/site/search']) ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
