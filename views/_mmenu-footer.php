9cbaa9265abf5c6afc3b18dfa7637482f832da3f
aileen <aileen.kingsler@mail.ru>
2019-12-25 13:12:32 +1000

    Убрала из mmenu блок с соцсетями

--- views/_mmenu-footer.php
+++ views/_mmenu-footer.php
@@ -48,60 +48,6 @@
     </div>
 </div>
 
-<div class="mmenu__social">
-    <div class="mmenu__social-title">Социальные сети</div>
-    <ul>
-        <li>
-            <a class="social-link social-link--vk" href="<?= Yii::$app->contactsManager->get('social_vkontakte') ?>" target="_blank">
-                <span class="sr-only">ВКонтакте</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
-                    <use xlink:href="/images/sprite.svg#icon-vk">
-                </svg>
-            </a>
-        </li>
-        <li>
-            <a class="social-link social-link--fb" href="<?= Yii::$app->contactsManager->get('social_facebook') ?>" target="_blank">
-                <span class="sr-only">Facebook</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
-                    <use xlink:href="/images/sprite.svg#icon-fb">
-                </svg>
-            </a>
-        </li>
-        <li>
-            <a class="social-link social-link--youtube" href="<?= Yii::$app->contactsManager->get('social_youtube') ?>" target="_blank">
-                <span class="sr-only">YouTube</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
-                    <use xlink:href="/images/sprite.svg#icon-youtube">
-                </svg>
-            </a>
-        </li>
-        <li>
-            <a class="social-link social-link--instargam" href="<?= Yii::$app->contactsManager->get('social_instagram') ?>" target="_blank">
-                <span class="sr-only">Instargam</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
-                    <use xlink:href="/images/sprite.svg#icon-instargam">
-                </svg>
-            </a>
-        </li>
-        <li>
-            <a class="social-link social-link--ok" href="#" target="_blank">
-                <span class="sr-only">OK</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
-                    <use xlink:href="/images/sprite.svg#icon-ok">
-                </svg>
-            </a>
-        </li>
-        <li>
-            <a class="social-link social-link--tw" href="#" target="_blank">
-                <span class="sr-only">Twitter</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" aria-hidden="true" role="presentation" focusable="false">
-                    <use xlink:href="/images/sprite.svg#icon-tw">
-                </svg>
-            </a>
-        </li>
-    </ul>
-</div>
-
 <a class="btn btn-primary btn-lg d-block m-auto ladda-button open-lead-modal" href="/leads/default/modal?key=callMe" data-target="#w2-callMe" data-toggle="modal" data-style="zoom-in">
     <span class="ladda-label">Заказать обратный звонок</span>
 </a>
