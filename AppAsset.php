3effe42270eeffa454c4da7ad4f3422c5707b68e
aileen <aileen.kingsler@mail.ru>
2019-12-27 15:08:03 +1000

    Добавила LeadFormAsset в зависимости

--- AppAsset.php
+++ AppAsset.php
@@ -4,7 +4,7 @@ namespace frontend\themes\woodland;
 
 use pantera\content\OwlCarouserAsset;
 use pantera\media\widgets\syncedOwls\SyncedOwlsAsset;
-use yii\bootstrap\BootstrapPluginAsset;
+use pantera\leads\widgets\form\LeadFormAsset;
 use yii\web\AssetBundle;
 
 /**
@@ -43,6 +43,7 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
         OwlCarouserAsset::class,
         SyncedOwlsAsset::class,
+        LeadFormAsset::class,
     ];
     public function init()
     {
