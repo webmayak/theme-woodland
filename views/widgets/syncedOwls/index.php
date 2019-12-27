0da4d7b68ee2cb3615a7a6a2ce0ae1f90d6da40e
Alex Kostin <web-mayak@mail.ru>
2019-12-27 13:32:21 +1000

    вьюшки для виджетов miniCart и syncedOwls

---
+++ views/widgets/syncedOwls/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: singletonn
+ * Date: 10/17/18
+ * Time: 3:54 PM
+ */
+
+use pantera\media\models\Media;
+use yii\web\View;
+
+/* @var $this View */
+/* @var $models Media[] */
+/* @var $showThumbs bool */
+?>
+<div class="synced-carousel-main owl-carousel">
+    <?php foreach ($models as $model): ?>
+        <div class="item">
+            <a data-fancybox="images" href="<?= $model->image() ?>">
+                <div class="image">
+                    <img src="<?= $model->image(730, 435, $this->context->cropImages) ?>" alt="">
+                </div>
+            </a>
+        </div>
+    <?php endforeach; ?>
+</div>
+<?php if ($showThumbs): ?>
+    <div class="synced-carousel-thumbs owl-carousel">
+        <?php foreach ($models as $model): ?>
+            <div class="item">
+                <div class="image">
+                    <img src="<?= $model->image(150, 100, $this->context->cropThumbs) ?>" alt="">
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
