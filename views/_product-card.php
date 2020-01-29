f639b16aa4c5a8bf4eb4d907ef37bb87bf01f233
Alex Kostin <web-mayak@mail.ru>
2020-01-29 23:12:11 +1000

    карточка проекта: выводим зачеркнутую цену если она больше основной

--- views/_product-card.php
+++ views/_product-card.php
@@ -56,7 +56,9 @@ use yii\helpers\Html;
         </li>
         <?php endif; ?>
     </ul>
+    <?php if ($model->default_price > $model->price): ?>
     <div class="product-card__old-price">Старая цена: <del><?= number_format($model->default_price, 0, ',', ' ') ?> Руб</del></div>
+    <?php endif; ?>
     <div class="product-card__price">Цена: от <b><?= number_format($model->price, 0, ',', ' ') ?> Руб.</b></div>
     <div class="product-card__actions">
         <a href="/<?=$model->present()->getUrl()?>" class="product-card__to-cart btn btn-primary">
