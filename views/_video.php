f98843e6225573ada101993914705167fdef000e
aileen <aileen.kingsler@mail.ru>
2020-01-29 14:17:08 +1000

    Вынесла видео на главной по управление из админки

--- views/_video.php
+++ views/_video.php
@@ -1,8 +1,13 @@
+<?php
+    use pantera\content\widgets\block\Block;
+?>
 <div class="video-block" id="video-block">
     <div class="container">
         <h2>Видео</h2>
         <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fVcxQFPvl88?rel=0" allowfullscreen></iframe>
+            <?= Block::widget([
+                'position' => 'front-video',
+            ]) ?>
         </div>
     </div>
 </div>
