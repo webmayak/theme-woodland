9c25241f38753feaace5274a99db1b6d06ffcee0
Alex Kostin <web-mayak@mail.ru>
2020-02-03 15:02:07 +1000

    подключена верстка для страницы галереи с выводом реальных картинок

--- views/catalog/category/index-gallery.php
+++ views/catalog/category/index-gallery.php
@@ -13,59 +13,31 @@ use pantera\leads\widgets\form\LeadForm;
 /* @var $this View */
 /* @var $links array */
 ?><main class="page-site-gallery-one__content">
-    <h1><?= $this->title ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php if ($model->media && is_array($model->media)): ?>
     <div class="row">
+        <?php foreach ($model->media as $index => $media): ?>
         <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
+            <a class="page-site-gallery-one__item-link gallery-link" href="<?= $media->image() ?>" data-fancybox="gallery">
+                <img class="page-site-gallery-one__img" src="<?= $media->image(255, 255, false) ?>" alt="<?= Html::encode($this->title) . ' - фото ' . ($index+1)  ?>">
             </a>
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6">
-            <a class="page-site-gallery-one__item-link gallery-link" href="/images/gallery-one-1.jpg" data-fancybox="gallery">
-                <img class="page-site-gallery-one__img" src="/images/gallery-one-1.jpg" alt="">
-            </a>
-        </div>
+        <?php endforeach; ?>
     </div>
+    <?php endif; ?>
+    <?php if (0): ?>
     <div class="page-site-gallery-one__btn-wrap text-center">
         <button class="btn btn-lg btn-primary">Показать еще</button>
     </div>
+    <?php endif; ?>
 </main>
 
 <div class="projects">
     <h2>Хиты продаж</h2>
     <?= \yii\widgets\ListView::widget([
         'dataProvider' => new \yii\data\ActiveDataProvider([
-            'query' => \common\modules\shop\models\ShopProduct::find()
+            'query' => \common\modules\shop\models\ShopProduct::find()->limit(10),
+            'pagination' => false,
         ]),
         'options' => [
             'class' => 'products-list',
@@ -74,13 +46,7 @@ use pantera\leads\widgets\form\LeadForm;
         'itemOptions' => [
             'class' => 'projects__item',
         ],
-        'layout' => '<div class="projects__carousel owl-carousel">{items}</div>{pager}',
-        'pager' => [
-            'class' => 'yii\bootstrap4\LinkPager',
-            'options' => [
-                'class' => 'text-center',
-            ],
-        ],
+        'layout' => '<div class="projects__carousel owl-carousel">{items}</div>',
     ]) ?>
 </div>
 
