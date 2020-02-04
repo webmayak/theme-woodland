3316069e1c227944352b291e1410d40e2fe3d2d7
Alex Kostin <web-mayak@mail.ru>
2020-02-04 15:50:24 +1000

    фикс карточки галереи на списке после перехода на multiple media

--- widgets/galleryList/views/_view.php
+++ widgets/galleryList/views/_view.php
@@ -13,6 +13,6 @@ use yii\web\View;
 /* @var $this View */
 /* @var $model CatalogCategory */
 ?><a class="page-site-gallery__item item-card" href="<?= $model->present()->getUrl() ?>">
-    <img class="item-card__img mb-3" src="<?= $model->media ? $model->media->image() : 'https://via.placeholder.com/300x200' ?>" alt="<?= Html::encode($model->name) ?>">
+    <img class="item-card__img mb-3" src="<?= ($model->media && is_array($model->media[0]) && !empty($model->media[0])) ? $model->media[0]->image() : 'https://via.placeholder.com/300x200' ?>" alt="<?= Html::encode($model->name) ?>">
     <h4 class="item-card__title mb-0"><?= Html::encode($model->name) ?></h4>
 </a>
