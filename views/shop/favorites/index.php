6132c490f05c6187f6bab57fca293872ccde841d
aileen <aileen.kingsler@mail.ru>
2020-02-14 14:03:01 +1000

    Вывела на странице избранного реальные данные

--- views/shop/favorites/index.php
+++ views/shop/favorites/index.php
@@ -1,12 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: singletonn
- * Date: 10/31/18
- * Time: 4:40 PM
- */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\web\View;
 
 /* @var $this View */
@@ -14,6 +9,30 @@ use yii\web\View;
 $this->title = 'Избранное';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<h1 class="title-home">
-    <?= $this->title ?>
-</h1>
+<div class="pb-5">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php if ($dataProvider->totalCount) : ?>
+        <?= \yii\widgets\ListView::widget([
+            'dataProvider' => $dataProvider,
+            'options' => [
+                'class' => 'products-list',
+            ],
+            'itemView' => '@theme/views/_product-card',
+            'itemOptions' => [
+                'class' => 'col-xl-3 col-lg-4 col-sm-6 catalog-page__item',
+            ],
+            'layout' => '<div class="row">{items}</div>{pager}',
+            'pager' => [
+                'class' => 'yii\bootstrap4\LinkPager',
+                'prevPageLabel' => '<i class="fa fa-angle-left"></i> Назад',
+                'nextPageLabel' => 'Вперед <i class="fa fa-angle-right"></i>',
+                'listOptions' => [
+                    'class' => ['pagination justify-content-center align-items-center']
+                ]
+            ],
+        ]) ?>
+    <?php else: ?>
+        <p class="text-center">Вы еще ничего не добавили в избранное, перейдите в <a href="<?= Url::to(['/shop/catalog/index']) ?>">каталог проектов</a></p>
+    <?php endif; ?>
+</div>
