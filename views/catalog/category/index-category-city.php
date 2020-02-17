2af9f64a2225e69e4ab96e112a436f47c1400038
aileen <aileen.kingsler@mail.ru>
2020-02-17 17:06:10 +1000

    Убрала вывод списка товаров в шаблоне страницы "категория-город"

--- views/catalog/category/index-category-city.php
+++ views/catalog/category/index-category-city.php
@@ -20,29 +20,6 @@ $this->title = $model->name;
         </div>
     </div>
     <?php endif; ?>
-
-    <?= \yii\widgets\ListView::widget([
-        'dataProvider' => new \yii\data\ActiveDataProvider([
-            'query' => \common\modules\shop\models\ShopProduct::find()
-        ]),
-        'options' => [
-            'class' => 'products-list',
-        ],
-        'itemView' => '@theme/views/_product-card',
-        'itemOptions' => [
-            'class' => 'col-xl-3 col-lg-4 col-sm-6 catalog-page__item',
-        ],
-        'layout' => '<div class="row">{items}</div>{pager}',
-        'pager' => [
-            'class' => 'yii\bootstrap4\LinkPager',
-            'prevPageLabel' => '<i class="fa fa-angle-left"></i> Назад',
-            'nextPageLabel' => 'Вперед <i class="fa fa-angle-right"></i>',
-            'listOptions' => [
-                'class' => ['page-search__pagination pagination justify-content-center align-items-center']
-            ]
-        ],
-    ]) ?>
-
 </main>
 
 </div><!-- закрываем .container -->
