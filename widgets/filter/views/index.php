6adfb954b6500679f97824a941cb13e07557d99a
Alex Kostin <web-mayak@mail.ru>
2020-02-17 20:05:11 +1000

    добавил trim для значений в виджете фильтра

--- widgets/filter/views/index.php
+++ widgets/filter/views/index.php
@@ -57,6 +57,7 @@ use yii\helpers\Html;
         <div class="filter__additional">
             <?php foreach ($attributes as $attribute) : ?>
                 <?php $default_values = preg_split('/\n+/', $attribute->default_values); ?>
+                <?php $default_values = array_map('trim', $default_values); ?>
                 <fieldset class="filter__additional-item">
                     <legend class="filter__additional-title"><?= $attribute->name ?>:</legend>
                     <div class="filter__content">
