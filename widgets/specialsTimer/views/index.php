6b62394a6c7424526039dac42b2f41861bf0717c
aileen <aileen.kingsler@mail.ru>
2020-02-18 12:23:37 +1000

    Создала виджет SpecialsTimer (пока без возможности задать дату)

---
+++ widgets/specialsTimer/views/index.php
@@ -0,0 +1,8 @@
+<div class="specials-timer">
+    <div class="specials-timer__due-date">
+        Акция действует до <b class="text-primary">25.10.2019</b>
+    </div>
+    <div class="specials-timer__days-left">
+        Осталось <b>12 дней</b>
+    </div>
+</div>
