4e5e21c00108de4a66cddd01a4ea01805aafd6d3
aileen <aileen.kingsler@mail.ru>
2020-02-18 13:28:25 +1000

    Добавили возможность задать дату окончания акции в виджете SpecialsTimer

--- widgets/specialsTimer/SpecialsTimer.php
+++ widgets/specialsTimer/SpecialsTimer.php
@@ -6,8 +6,32 @@ use yii\base\Widget;
 
 class SpecialsTimer extends Widget
 {
-    public function run()
+    /* @var string */
+    public $date;
+
+    /**
+     * @return string
+     */
+    public function run() : string
     {
-        return $this->render('index');
+        parent::run();
+        $dateISO = preg_replace('/^(\d{2})\.(\d{2})\.(\d{4})$/', '$3-$2-$1', $this->date);
+        $dateTimestamp = strtotime($dateISO . ' 23:59:59');
+        $nowTimestamp = time();
+        $diff = $dateTimestamp - $nowTimestamp;
+        return $this->render('index', [
+            'date' => $this->date,
+            'timeLeft' => $this->formatTimeLeft($diff),
+        ]);
+    }
+
+    /**
+     * @param $seconds
+     * @return string
+     */
+    private function formatTimeLeft($seconds) : string
+    {
+        $daysLeft = ceil($seconds / 86400);
+        return $daysLeft . ' дней';
     }
 }
--- widgets/specialsTimer/views/index.php
+++ widgets/specialsTimer/views/index.php
@@ -1,8 +1,8 @@
 <div class="specials-timer">
     <div class="specials-timer__due-date">
-        Акция действует до <b class="text-primary">25.10.2019</b>
+        Акция действует до <b class="text-primary"><?= $date ?></b>
     </div>
     <div class="specials-timer__days-left">
-        Осталось <b>12 дней</b>
+        Осталось <b><?= $timeLeft ?></b>
     </div>
 </div>
