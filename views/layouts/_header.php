6543846f0eccf86bb54ad1aa2e7a3581fcc0cb55
Alex Kostin <web-mayak@mail.ru>
2020-03-02 20:22:21 +1000

    не выводим телефон в шапке если он не указан в настройках

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -83,24 +83,31 @@ use pantera\geolocation\widgets\geolocation\Geolocation;
                             <div>
                                 <div class="contact-item__key">Телефон:</div>
                                 <div class="contact-item__value">
+                                    <?php
+                                        $phone_sales_department = Yii::$app->contactsManager->get('phone_sales_department');
+                                        $phone_hotline = Yii::$app->contactsManager->get('phone_hotline');
+                                        $showDropdown = $phone_sales_department || $phone_hotline;
+                                    ?>
                                     <div class="dropdown">
-                                        <button class="contact-item__toggle dropdown-toggle" type="button" data-toggle="dropdown">
-                                            <?= Yii::$app->contactsManager->get('phone_main') ?> <i class="fa fa-chevron-down"></i>
+                                        <button class="contact-item__toggle<?php if ($showDropdown) : ?> dropdown-toggle<?php endif; ?>" type="button"<?php if ($showDropdown) : ?> data-toggle="dropdown"<?php endif; ?>>
+                                            <?= Yii::$app->contactsManager->get('phone_main') ?><?php if ($showDropdown) : ?> <i class="fa fa-chevron-down"></i><?php endif; ?>
                                         </button>
+                                        <?php if ($showDropdown) : ?>
                                         <ul class="tel-dropdown dropdown-menu">
+                                            <?php if ($phone_sales_department) : ?>
                                             <li>
                                                 Отдел Продаж:
-                                                <a href="tel:<?= Yii::$app->contactsManager->get('phone_sales_department') ?>">
-                                                    <?= Yii::$app->contactsManager->get('phone_sales_department') ?>
-                                                </a>
+                                                <a href="tel:<?= $phone_sales_department ?>"><?= $phone_sales_department ?></a>
                                             </li>
+                                            <?php endif; ?>
+                                            <?php if ($phone_hotline) : ?>
                                             <li>
                                                 Горячая Линия:
-                                                <a href="tel:<?= Yii::$app->contactsManager->get('phone_hotline') ?>">
-                                                    <?= Yii::$app->contactsManager->get('phone_hotline') ?>
-                                                </a>
+                                                <a href="tel:<?= $phone_hotline ?>"><?= $phone_hotline ?></a>
                                             </li>
+                                            <?php endif; ?>
                                         </ul>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
