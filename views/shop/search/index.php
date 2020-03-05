84178b8871c2fd54ca61ad9ed4d52a8fca89cf5e
Alex Kostin <web-mayak@mail.ru>
2020-03-05 16:17:01 +1000

    вью страницы поиска: немного рефакторинга

--- views/shop/search/index.php
+++ views/shop/search/index.php
@@ -12,23 +12,21 @@ $this->title = $query ? '"' . $query . '" - результаты поиска' :
 $this->params['breadcrumbs'][] = $defaultTitle;
 
 /* @var $this View */
-?><main class="page-search__content">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <form action="<?= Url::to(['/shop/search/index']) ?>" method="get">
-        <div class="input-group">
-            <input class="page-search__search-field form-control form-control-lg" type="text" name="q" placeholder="Поиск по сайту" value="<?= Html::encode($query) ?>">
-            <div class="input-group-append">
-              <button type="submit" class="page-search__search-btn btn btn-primary">Поиск</button>
-            </div>
+?>
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<form action="<?= Url::to(['/shop/search/index']) ?>" method="get">
+    <div class="input-group">
+        <input class="page-search__search-field form-control form-control-lg" type="text" name="q" placeholder="Поиск по сайту" value="<?= Html::encode($query) ?>">
+        <div class="input-group-append">
+          <button type="submit" class="page-search__search-btn btn btn-primary">Поиск</button>
         </div>
-    </form>
-    <div class="page-search__text">Если результаты вас не удовлетворяют, пожалуйста, попробуйте еще раз</div>
-    <?= ProductsList::widget([
-        'dataProvider' => $dataProvider,
-    ]) ?>
-    <?php if (0): ?>
-    <div class="page-search__btn-wrap text-center">
-        <button class="btn btn-lg btn-primary">Показать еще</button>
     </div>
-    <?php endif; ?>
-</main>
+</form>
+
+<div class="page-search__text">Если результаты вас не удовлетворяют, пожалуйста, попробуйте еще раз</div>
+
+<?= ProductsList::widget([
+    'dataProvider' => $dataProvider,
+]) ?>
