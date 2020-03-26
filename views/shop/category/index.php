1f733c01dbe1f506d20b4d69a072833932bd5d3b
Alex Kostin <web-mayak@mail.ru>
2020-03-26 18:58:13 +1000

    вывод h1 на странице категории с учетом поля seo

--- views/shop/category/index.php
+++ views/shop/category/index.php
@@ -4,14 +4,14 @@ use yii\helpers\Html;
 use frontend\themes\woodland\widgets\shopProducts\ProductsPjaxList;
 use frontend\themes\woodland\widgets\filter\Filter as ProductsFilter;
 
-$this->title = $model->name;
+$this->title = $model->seo->h1 ?? $model->name;
 
 $this->params['breadcrumbs'][] = ['label' => 'Каталог', 'url' => ['/shop/catalog/index']];
 foreach ($model->parents as $key => $parent) {
     if ($key == 0) continue;
     $this->params['breadcrumbs'][] = ['label' => $parent->name, 'url' => $parent->present()->getUrl()];
 }
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $model->name;
 
 $isProjectsCategory = !preg_match('/(sadovyj-dekor|pellety)/', Yii::$app->request->pathInfo);
 
