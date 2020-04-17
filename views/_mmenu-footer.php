ffe8b9ca2f7c2b21618fe5e01e23ee136e075a8d
aileen <aileen.kingsler@mail.ru>
2020-04-17 13:13:06 +1000

    Настроила вывод контактов в мобильном меню из настроек

--- views/_mmenu-footer.php
+++ views/_mmenu-footer.php
@@ -7,18 +7,31 @@
     <div>
         <div class="contact-item__key">Телефон:</div>
         <div class="contact-item__value">
+            <?php
+            $phone_sales_department = Yii::$app->contactsManager->get('phone_sales_department');
+            $phone_hotline = Yii::$app->contactsManager->get('phone_hotline');
+            $showDropdown = $phone_sales_department || $phone_hotline;
+            ?>
             <div class="dropdown">
-                <button class="contact-item__toggle dropdown-toggle" type="button" data-toggle="dropdown">8-800-775-18-13 <i class="fa fa-chevron-down"></i></button>
-                <ul class="tel-dropdown dropdown-menu">
-                    <li>
-                        Отдел Продаж:
-                        <a href="tel:+74957751813">+7-495-775-18-13</a>
-                    </li>
-                    <li>
-                        Горячая Линия:
-                        <a href="tel:89267751813">8-926-775-18-13</a>
-                    </li>
-                </ul>
+                <button class="contact-item__toggle<?php if ($showDropdown) : ?> dropdown-toggle<?php endif; ?>" type="button"<?php if ($showDropdown) : ?> data-toggle="dropdown"<?php endif; ?>>
+                    <?= Yii::$app->contactsManager->get('phone_main') ?><?php if ($showDropdown) : ?> <i class="fa fa-chevron-down"></i><?php endif; ?>
+                </button>
+                <?php if ($showDropdown) : ?>
+                    <ul class="tel-dropdown dropdown-menu">
+                        <?php if ($phone_sales_department) : ?>
+                            <li>
+                                Отдел Продаж:
+                                <a href="tel:<?= $phone_sales_department ?>"><?= $phone_sales_department ?></a>
+                            </li>
+                        <?php endif; ?>
+                        <?php if ($phone_hotline) : ?>
+                            <li>
+                                Горячая Линия:
+                                <a href="tel:<?= $phone_hotline ?>"><?= $phone_hotline ?></a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -32,7 +45,7 @@
     </div>
     <div>
         <div class="contact-item__key">Email:</div>
-        <div class="contact-item__value">fortuna-les@gmail.com</div>
+        <div class="contact-item__value"><?= Yii::$app->contactsManager->get('email') ?></div>
     </div>
 </div>
 
@@ -44,7 +57,7 @@
     </div>
     <div>
         <div class="contact-item__key">Адрес:</div>
-        <div class="contact-item__value">г. Москва, ул. Летчика Бабушкина, д. 123, оф. 244</div>
+        <div class="contact-item__value"><?= Yii::$app->contactsManager->get('address') ?></div>
     </div>
 </div>
 
