44ec02bdae078bea5d089966e40b61f13f23cc22
Alex Kostin <web-mayak@mail.ru>
2020-07-10 15:59:18 +1000

    перенес из шаблона в админку разметку контейнера youtube

--- views/_video.php
+++ views/_video.php
@@ -6,10 +6,8 @@ use pantera\content\widgets\block\Block;
 <div class="video-block" id="video-block">
     <div class="container">
         <h2>Видео</h2>
-        <div class="embed-responsive embed-responsive-16by9">
-            <?= Block::widget([
-                'position' => 'front-video',
-            ]) ?>
-        </div>
+        <?= Block::widget([
+            'position' => 'front-video',
+        ]) ?>
     </div>
 </div>
