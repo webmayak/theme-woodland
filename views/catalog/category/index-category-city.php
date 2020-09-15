b3a34e8c3c6dcd08637067c189293a60b3331d07
Alex Kostin <web-mayak@mail.ru>
2020-09-15 22:42:24 +1000

    на странице "категория-город" h1 и title выводим из модуля seo

--- views/catalog/category/index-category-city.php
+++ views/catalog/category/index-category-city.php
@@ -6,10 +6,10 @@ use yii\helpers\Html;
 use yii\web\View;
 
 /* @var $this View */
-$this->title = $model->name;
+$this->title = Yii::$app->seo->title ?? $model->name;
 
 ?><main class="page-site-category__content">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode(Yii::$app->seo->h1 ?? $model->name) ?></h1>
 
     <?php if ($model->description) : ?>
     <div class="text-block">
