489012db831d5428ca4d9946905e94b0d067e2f7
aileen <aileen.kingsler@mail.ru>
2021-02-05 13:58:24 +1000

    Поправила вёрстку, чтобы с картой можно было взаимодействовать

--- views/_works-map--with-form.php
+++ views/_works-map--with-form.php
@@ -7,19 +7,17 @@ use pantera\leads\widgets\form\LeadForm;
     <div class="container">
         <h2>Карта выполненных объектов</h2>
     </div>
-    <div class="works-map__map-wrap">
+    <div class="position-relative">
         <div class="works-map__form">
-            <div class="container">
-                <div class="works-map__form-block form-alert alert alert-dismissible fade show">
-                    <div class="form-alert__title">Заказать бесплатный звонок</div>
-                    <?= LeadForm::widget([
-                        'key' => 'orderOnMap',
-                        'mode' => LeadForm::MODE_INLINE,
-                    ]) ?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+            <div class="form-alert alert alert-dismissible fade show">
+                <div class="form-alert__title">Заказать бесплатный звонок</div>
+                <?= LeadForm::widget([
+                    'key' => 'orderOnMap',
+                    'mode' => LeadForm::MODE_INLINE,
+                ]) ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
         </div>
         <?= !empty($mapScript) ? $mapScript : '<iframe src="https://yandex.ru/map-widget/v1/-/CGXCfH1r"></iframe>' ?>
